57b61e9da3e4fbd57efc4ed4d675183dfa1019c7
Peter <petermach47@gmail.com>
2023-11-15 20:44:47 +0300

    open clicked elements in new tab

--- auto-refresh-click.js
+++ auto-refresh-click.js
@@ -30,7 +30,7 @@ puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
   await page.goto('https://platform.verbit.co/');
 
   const username = await page.waitForSelector('form > div.sc-bkkeKt.hlZVeT > div.sc-jrQzAO.cCmSqm > input');
-  await username.type('johnsonterry16541@gmail.com'); //Nestanie123@
+  await username.type('janellporter.125@gmail.com'); //Nestanie123@
   const next = await page.waitForSelector('form > button');
   await next.click();
 
@@ -39,6 +39,35 @@ puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
   await page.waitForNavigation({ waitUntil: 'load' });
   
     // const requestLimit = await page.waitForSelector('div.request-limit.col-md-6.col-md-offset-3 > div > div.request-limit__title');  
+    
+    async function getNewBrowserTab(browser) {
+      let resultPromise
+
+      async function onTargetcreatedHandler(target) {
+          if (target.type() === 'page') {
+              const newPage = await target.page()
+              const newPagePromise = new Promise(y =>
+                  newPage.once('domcontentloaded', () => y(newPage))
+              )
+
+              const isPageLoaded = await newPage.evaluate(
+                  () => document.readyState
+              )
+
+              browser.off('targetcreated', onTargetcreatedHandler) // unsubscribing
+
+              return isPageLoaded.match('complete|interactive')
+                  ? resultPromise(newPage)
+                  : resultPromise(newPagePromise)
+          }
+      }
+
+      return new Promise(resolve => {
+          resultPromise = resolve
+          browser.on('targetcreated', onTargetcreatedHandler)
+      })
+  }
+    
     let firstInterval;
 
     async function reload() {
@@ -48,13 +77,45 @@ puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
         console.log("Successfully reloaded:", currentUrl);
         await page.reload();
 
-         // Replace 'your-class-name' with the actual class name of the elements you want to click
-          const transcriptionTasks = await page.$$('#transcription_jobs_table > .action-edit');
+        // ensure that the navigation is complete before attempting to interact with the element.
+        await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+        // await page.waitForSelector('div > span.filename.css_ellipsis-part2', { timeout: 60000 });
 
-          for (const taskId of transcriptionTasks) {
-            await taskId.click();
-            // Add any additional actions or waiting as needed
-          }
+        console.log('Before waiting for selector');
+const xpathSelector = '//div/span[@class="filename css_ellipsis-part2"]';
+
+// Wait for the elements to be present
+await page.waitForXPath(xpathSelector, { timeout: 120000 });
+console.log('After waiting for selector');
+
+// Get all matching elements
+const transcriptionTasks = await page.$x(xpathSelector);
+
+// Create a Set to keep track of clicked elements
+const clickedElements = new Set();
+
+for (const taskId of transcriptionTasks) {
+    // Get the XPath of the current element
+    const elementXPath = await page.evaluate(el => {
+        const { xpath } = el;
+        return xpath;
+    }, taskId);
+
+    // Check if the element has already been clicked
+    if (!clickedElements.has(elementXPath)) {
+        // Click on the element
+        await taskId.click({ button: 'middle' });
+
+        // Add the XPath to the set to mark it as clicked
+        clickedElements.add(elementXPath);
+
+        // Perform additional actions if needed (e.g., opening a new page)
+        const newPage = await getNewBrowserTab(browser);
+        await newPage.bringToFront();
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+}
+
         
       } else {
         clearInterval(firstInterval);
